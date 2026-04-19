fe0a2ca151a9a6dcc4e1ea46375287297c939b20
Kaya-12345 <purpledog13131@gmail.com>
2026-04-18 17:44:43 -0700

    Update while_loop.py

---
+++ lesson9/classwork/while_loop.py
@@ -0,0 +1,17 @@
+# for loop
+x = 0
+for i in range(5):
+  x += 1
+
+# while loop
+i = 10
+while (i >10):
+  print(i)
+  i = i -1 # i goes down by 1
+
+print(i)
+
+i = i
+while i < 5:
+  print(i)
+  i = i + 1
