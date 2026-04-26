837dded636f12accd251fc77b48c17f45efee757
Kaya-12345 <purpledog13131@gmail.com>
2026-04-25 17:09:10 -0700

    Update variables.py

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,10 @@
+x = 5 # number: whole-number value
+print(x)
+
+number = 3.14 # number: decimal value
+print(number)
+
+favorite_food = "xxx" # string: text
+print("My favorite food is" + favorite_food)
+
+print(3.18 + "dogs" + x) # string concatenation
